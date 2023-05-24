4f5283850fc15a2379685ff7bd9c3c4f9418194a
DavidBoreda <davidboreda@gmail.com>
2023-05-24 09:29:26 +0300

    AboutUs

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -6,8 +6,14 @@ const AboutUs = () => {
       <h1>About Us</h1>
       <CardComponent
         title="Shopeee"
-        description="xyz"
-        imgUrl="https://openai-labs-public-images-prod.azureedge.net/user-22F4rDgUJx2yMY14KAMAMOF4/generations/generation-84l4RJDgNoHA21qJAQ90VqgP/image.png"
+        description="This website, which is the final project of the full-stack course, reflects the variety of technologies and tools I acquired during the months of the course.
+The site simulates a virtual store, there are two types of user - customer and admin.
+You can add products to a list of favorites as well as place an order for registered customers.
+You can view the favorite lists.
+A person with admin privileges can perform a crud operation on both products and clients.
+
+The website is built in REACT and the server side in NODEJS, the database is mongoDB."
+        imgUrl=""
       />
     </div>
   );
